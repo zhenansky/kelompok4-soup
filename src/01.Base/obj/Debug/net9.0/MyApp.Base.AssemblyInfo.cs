f6b16285c38f470478a68a9346f30fc3ac28d963
ADI DHARMA PUTRA <adidarmap@gmail.com>
2025-09-19 22:32:50 +0700

    Add login.css and login.html for login page

--- src/01.Base/obj/Debug/net9.0/MyApp.Base.AssemblyInfo.cs
+++ src/01.Base/obj/Debug/net9.0/MyApp.Base.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Base")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5327a831407c72572410606840f16f05c14bda8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Base")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Base")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/02.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
+++ src/02.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5327a831407c72572410606840f16f05c14bda8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
