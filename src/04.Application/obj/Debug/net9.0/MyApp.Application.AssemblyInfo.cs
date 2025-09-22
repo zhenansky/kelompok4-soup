fb363baf2500a3baf393ab570a885012534e05de
ADI DHARMA PUTRA <adidarmap@gmail.com>
2025-09-22 16:02:55 +0700

    Deskripsi perubahan

--- src/04.Application/obj/Debug/net9.0/MyApp.Application.AssemblyInfo.cs
+++ src/04.Application/obj/Debug/net9.0/MyApp.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beff8c28d61b24ff2f3d4d42f57fc646be6a43c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44aa37923a025ace5d54d578d0df3a1ba4f8523d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
